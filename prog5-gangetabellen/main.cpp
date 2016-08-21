a154685437c560fff59238d0452f8f30eac8d071
olemcDEV <olemagca@gmail.com>
2016-08-21 18:37:58 +0200

    odde og partall justeringer

--- prog5-gangetabellen/main.cpp
+++ prog5-gangetabellen/main.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main() {
+    //enum tallType {oddeTall, parTall}; Hvordan kan jeg bruke dette?
     for (int i = 1; i <= 10; i++) {
         for (int j = 1; j <= 10; j++) {
-           cout
-                << (i*j) << " ";
+            if (i*j % 2 == 0 || i*j % 2 == 1) {
+                cout
+                    << (i*j) << " ";
+            }
         }
         cout
             <<
