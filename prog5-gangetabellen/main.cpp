e1d7d0e2eed4129a057e77955cfe35d487d46de7
olemc <olemc@DESKTOP-NC0NDLA>
2016-08-22 12:21:46 +0200

    enum added

--- prog5-gangetabellen/main.cpp
+++ prog5-gangetabellen/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main() {
-    //enum tallType {oddeTall, parTall}; Hvordan kan jeg bruke dette?
+    enum tallType {oddeTall, parTall}; //Hvordan kan jeg bruke dette?
     for (int i = 1; i <= 10; i++) {
         for (int j = 1; j <= 10; j++) {
-            if (i*j % 2 == 0 || i*j % 2 == 1) {
+			if (i*j % 2 == tallType{ oddeTall } || i*j % 2 == tallType{ parTall }) {
                 cout
                     << (i*j) << " ";
             }
