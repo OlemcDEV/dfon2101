96956325c9fe95ad9bf653548c42fc0de07f7df9
olemcDEV <olemcdev@gmail.com>
2016-08-23 15:07:58 +0200

    Lagt til gender

--- prog6-struct/main.cpp
+++ prog6-struct/main.cpp
@@ -2,21 +2,39 @@
 using namespace std;
 
 struct menneske {
-	char kjønn;
-	int alder;
-	float hoeyde;
+	char gender;
+	int age;
+	float height;
 } 
 
 person;
 
 int main() {
+
+	do
+	{
+        cout
+            << "Hello human, what is your gender? Type m or f"
+            << endl;
+		cin
+			>>  
+            person.gender;
+    } while (person.gender != 'm'&&'M'&&'f'&&'F');
 	cout
-		<< "Hei menneske, hva er ditt kjønn?"
+		<< person.gender
 		<< endl;
-	cin
-		>> person.kjønn;
-	cout
-		<< person.kjønn
-		<< endl;
-	system("pause");
+
+    do
+    {
+        cout
+            << "What is your age?"
+            << endl;
+        cin
+            >>
+            person.age;
+    } while (person.age = NULL);
+    cout
+        << person.age
+        << endl;
+    system("pause");
 }
