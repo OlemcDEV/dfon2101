e72d94c902257efd313c6fbb7c02e6049a38e0c2
olemcDEV <olemagca@gmail.com>
2016-08-25 23:01:18 +0200

    Backup av kalk

--- prog7_kalk/main.cpp
+++ prog7_kalk/main.cpp
@@ -1,75 +1,93 @@
 #include <iostream>
 #include <algorithm>
 void sumOf2 () {
-    double tall1;
-    double tall2;
+    double dNumber1;
+    double dNumber2;
 
     std::cout
         << "Sum of numbers, type first number: "
         << std::endl;
     std::cin
-        >> tall1;
+        >> dNumber1;
 
     std::cout
         << "Sum of numbers, second number: "
         << std::endl;
     std::cin
-        >> tall2;
+        >> dNumber2;
 
     std::cout
         << "Sum of these numbers is: "
-        << (tall1 + tall2)
+        << (dNumber1 + dNumber2)
         << std::endl;
 
 }
 char op;
 int position;
+int opCount;
+double dNumber1;
+double dNumber2;
 
-int findOp (char stykke[]) {
-    position;
-    op;
-    for (int i = 0; i < sizeof(stykke); i++) {
-        if (stykke[i] == '+') {
-            std::cout
-                << "Vi fant et + tegn."
-                << std::endl;
-            position = i;
-            op = '+';
-            return op,position;
+void findOpCount(char array[]) {
+    char x = 0;
+    opCount;
+    for (int i = 0; i < sizeof(array); i++) {
+        x = array[i];
+        switch (x) {
+        case '+': opCount++;
+            break;
+        case '-': opCount++;
+            break;
+        case '*': opCount++;
+            break;
+        case '/': opCount++;
+            break;
         }
-        else if (stykke[i] == '-') {
-            std::cout
-                << "Vi fant et - tegn."
-                << std::endl;
-            position = i;
-            op = '-';
-            return op, position;
+    }
+    std::cout
+        << "op count/case: "
+        << opCount
+        << std::endl;
+}
+void findOpAndSplitAndDoTheMath(char array[]) {
+    switch (opCount) {
+    case 0:
+        std::cout
+            << array
+            << std::endl;
+        break;
+    case 1:
+        //Finne posisjonen til operatoren
+        for (int i = 0; i <= sizeof(array[i]); i++) {
+            if (array[i] = '+' || '-' || '*' || '/') {
+                position = i;
+                for (int j = 0; j < sizeof(array[j]); j++) {
+                    position = i;
+                }
+                std::cout
+                    //<< dNumber1
+                    << std::endl;
+            }
         }
-        else if (stykke[i] == '*') {
-            std::cout
-                << "Vi fant et * tegn."
-                << std::endl;
-            position = i;
-            op = '*';
-            return op, position;
-        }
-        else if (stykke[i] == '/') {
-            std::cout
-                << "Vi fant et / tegn."
-                << std::endl;
-            position = i;
-            op = '/';
-            return op, position;
-        }
+
+        break;
+    case 2:
+        break;
+    case 3:
+        std::cout
+            << "ERROR 001: For a calc a(+,-,*,/)b, "
+            "it can't be more than 2 operators";
+        break;
     }
 }
 
 void sumOf2_withOperator() {
 
-    double tall1 = 0;
-    double tall2 = 0;
+    double dNumber1 = 0;
+    double dNumber2 = 0;
     double answer = 0;
     char input1[100];
+
     std::cout
         << "Type in the calculation f.example: 3.12+2.12 or -3.12-2.12"
         << std::endl;
@@ -80,65 +98,9 @@ void sumOf2_withOperator() {
         << "Here's what you want to calculate: "
         << input1
         << std::endl;
-    findOp(input1);
-    if (op == '+') {
-        std::cout
-            << "We know there's a + sign in method sumOf2_withOperator"
-            << std::endl
-            << "position to the operator is: "
-            << position
-            << std::endl;
-    }
-    else if (op == '-') {
-        std::cout
-            << "We know there's a - sign in method sumOf2_withOperator"
-            << std::endl
-            << "position to the operator is: "
-            << position
-            << std::endl;
-    }
-    else if (op == '*') {
-        std::cout
-            << "We know there's a * sign in method sumOf2_withOperator"
-            << std::endl
-            << "position to the operator is: "
-            << position
-            << std::endl;
-    }
-    else if (op == '/') {
-        std::cout
-            << "We know there's a / sign in method sumOf2_withOperator"
-            << std::endl
-            << "position to the operator is: "
-            << position
-            << std::endl;
-    }
-    std::cout
-        << "Hei.....................................";
-    for (int i = 0; i < sizeof(input1); i++) {
-        do {
-            tall1 += input1[i];
-        } while (i != position);
 
-        std::cout
-            << "hei 2";
-
-        do {
-            tall2 += input1[position + i];
-        } while (i != sizeof(input1));
-    }
-    std::cout
-        << "Hei";
-
-    switch (op) {
-    case '+': answer = tall1 + tall2;
-        std::cout
-            << answer
-            << std::endl;
-        break;
-    }
-
-    
+    findOpCount(input1);
+    findOpAndSplitAndDoTheMath(input1);
 }
 
 int main() {
@@ -150,6 +112,4 @@ int main() {
     sumOf2_withOperator();
 
     system("pause");
-
-
 }
