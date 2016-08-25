8a0d552594041265fba888cade9744f64717d260
olemcDEV <olemcdev@gmail.com>
2016-08-25 17:47:26 +0200

    nyeste

--- prog7_kalk/main.cpp
+++ prog7_kalk/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <algorithm>
 void sumOf2 () {
     double tall1;
     double tall2;
@@ -22,10 +22,54 @@ void sumOf2 () {
         << std::endl;
 
 }
+char op;
+int position;
 
-void sumOf2_withOperators() {
-    char input1[9000];
+int findOp (char stykke[]) {
+    position;
+    op;
+    for (int i = 0; i < sizeof(stykke); i++) {
+        if (stykke[i] == '+') {
+            std::cout
+                << "Vi fant et + tegn."
+                << std::endl;
+            position = i;
+            op = '+';
+            return op,position;
+        }
+        else if (stykke[i] == '-') {
+            std::cout
+                << "Vi fant et - tegn."
+                << std::endl;
+            position = i;
+            op = '-';
+            return op, position;
+        }
+        else if (stykke[i] == '*') {
+            std::cout
+                << "Vi fant et * tegn."
+                << std::endl;
+            position = i;
+            op = '*';
+            return op, position;
+        }
+        else if (stykke[i] == '/') {
+            std::cout
+                << "Vi fant et / tegn."
+                << std::endl;
+            position = i;
+            op = '/';
+            return op, position;
+        }
+    }
+}
 
+void sumOf2_withOperator() {
+
+    double tall1 = 0;
+    double tall2 = 0;
+    double answer = 0;
+    char input1[100];
     std::cout
         << "Type in the calculation f.example: 3.12+2.12 or -3.12-2.12"
         << std::endl;
@@ -33,13 +77,68 @@ void sumOf2_withOperators() {
         >>
         input1;
     std::cout
-        << "Here's your calculation: "
+        << "Here's what you want to calculate: "
         << input1
         << std::endl;
+    findOp(input1);
+    if (op == '+') {
+        std::cout
+            << "We know there's a + sign in method sumOf2_withOperator"
+            << std::endl
+            << "position to the operator is: "
+            << position
+            << std::endl;
+    }
+    else if (op == '-') {
+        std::cout
+            << "We know there's a - sign in method sumOf2_withOperator"
+            << std::endl
+            << "position to the operator is: "
+            << position
+            << std::endl;
+    }
+    else if (op == '*') {
+        std::cout
+            << "We know there's a * sign in method sumOf2_withOperator"
+            << std::endl
+            << "position to the operator is: "
+            << position
+            << std::endl;
+    }
+    else if (op == '/') {
+        std::cout
+            << "We know there's a / sign in method sumOf2_withOperator"
+            << std::endl
+            << "position to the operator is: "
+            << position
+            << std::endl;
+    }
     std::cout
-        << "Answer is: "
-        
+        << "Hei.....................................";
+    for (int i = 0; i < sizeof(input1); i++) {
+        do {
+            tall1 += input1[i];
+        } while (i != position);
 
+        std::cout
+            << "hei 2";
+
+        do {
+            tall2 += input1[position + i];
+        } while (i != sizeof(input1));
+    }
+    std::cout
+        << "Hei";
+
+    switch (op) {
+    case '+': answer = tall1 + tall2;
+        std::cout
+            << answer
+            << std::endl;
+        break;
+    }
+
+    
 }
 
 int main() {
@@ -48,8 +147,7 @@ int main() {
     //sumOf2();
 
     //DEL 2
-    sumOf2_withOperators();
-
+    sumOf2_withOperator();
 
     system("pause");
 
