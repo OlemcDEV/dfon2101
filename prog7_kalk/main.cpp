36b1b2658941d645e639b58ccdadc8d7af41c23b
olemcDEV <olemagca@gmail.com>
2016-08-26 11:30:59 +0200

    Latest work

--- prog7_kalk/main.cpp
+++ prog7_kalk/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <algorithm>
+#include <stdlib.h>
+#include <string>
 
 void sumOf2 () {
     double dNumber1;
@@ -29,11 +30,10 @@ int opCount;
 double dNumber1;
 double dNumber2;
 
+//Function to find how many operators (+,-,*,/) there are in the array
 void findOpCount(char array[]) {
     char x = 0;
     opCount;
-    op;
-    position;
     for (int i = 0; i < strlen(array); i++) {
         x = array[i];
         switch (x) {
@@ -52,49 +52,92 @@ void findOpCount(char array[]) {
         << opCount
         << std::endl;
 }
+
+/*
+Function to find the operator closest to middle, split the numbers on each
+side of the operator Example: (22.2)+(33.3) into 2 variables for so to 
+calculate the 2 variables depending on which operator is current.
+*/
 void findOpAndSplitAndDoTheMath(char array[]) {
     position;
+    std::string str1;
+    std::string str2;
     switch (opCount) {
     case 0:
         std::cout
             << array
             << std::endl;
         break;
-    case 1:
-        //Finner index til operatoren
+        //case1 = when there's only 1 operator in the input
+    case 1: {
+        //Finding the index of operator and what kind of operator it is
         for (int j = 0; j < strlen(array); j++) {
             char x = array[j];
             switch (x) {
             case '+':
                 position = j;
                 break;
+                op = '+';
             case '-':
                 position = j;
                 break;
+                op = '-';
             case '*':
                 position = j;
                 break;
+                op = '*';
             case '/':
                 position = j;
                 break;
+                op = '/';
             }
         }
+        /* NOT CURRENTLEY WORKING!!!
+        Problems are starting here.. Trying to solve the mystery of how to
+        split calculation into 2 variables */
+        char dArray1[100];
+        char dArray2[100];
+        for (int i = 0; i < position; i++) {
+            dArray1[i] = array[i];
+        }
+        for (int i = position+1; i < 100-position; i++) {
+            dArray2[i] = array[i];
+        }
+        str1 = std::string(dArray1);
+        str2 = std::string(dArray2);
+        std::cout
+            << str1
+            //<< str2
+            << std::endl;
+    }
         std::cout
             << "Index of op: "
             << position
+            << std::endl
+            << "dNumber1: "
+            << dNumber1
+            << std::endl
+            << "dNumber2: "
+            << dNumber2
             << std::endl;
         break;
     case 2:
         break;
-    case 3:
+    default:
         std::cout
             << "ERROR 001: For a calc a(+,-,*,/)b, "
-            "it can't be more than 2 operators";
+            " where a and b are positive, it can't be more than 2 operators"
+            << std::endl;
         break;
         }
 }
 
-void sumOf2_withOperator() {
+int factorial(int n)
+{
+    return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+}
+
+void calc() {
 
     double dNumber1 = 0;
     double dNumber2 = 0;
@@ -112,16 +155,35 @@ void sumOf2_withOperator() {
         << input1
         << std::endl;
 
-    findOpCount(input1);
-    findOpAndSplitAndDoTheMath(input1);
+    
+
+    //Do factorial - funker bare med et siffer så langt
+    if (input1[1] == '!') {
+
+        int c = int(input1[0] - '0');
+        std::cout
+            << "Factorial of "
+            << c
+            << " is: "
+            << factorial(c)
+            << std::endl;
+    }
+    //Do normal job
+    else {
+        findOpCount(input1);
+        findOpAndSplitAndDoTheMath(input1);
+    }
+
 }
 
 int main() {
     //DEL 1
     //sumOf2();
 
-    //DEL 2
-    sumOf2_withOperator();
+    //DEL 2,3,4 og 5
+    calc();
 
+    //DEL 6
+    //DEL 7
     system("pause");
 }
